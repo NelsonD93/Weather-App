260c6285effd5a07cd0b019a2e1dad8ea6bde6ac
Nelson Drozd <nelsondrozd@gmail.com>
2022-08-26 23:13:38 -0400

    Formatted script and HTML, started adding function to display 5-day forecast

--- assets/script.js
+++ assets/script.js
@@ -11,42 +11,49 @@ submitBtn.addEventListener("click", function () {
 
 
 function geoLocation(cityName) {
-    var geoCodeApi = 'https://api.openweathermap.org/geo/direct?1.0/q='+ cityName + '&limit=1&appid=' + apiKey;
+    var geoCodeApi = 'http://api.openweathermap.org/geo/1.0/direct?q=' + cityName + '&limit=1&appid=' + apiKey;
 
     console.log(geoCodeApi)
 
     fetch(geoCodeApi)
-    .then((response) => {
-        console.log(response)
-        return response.json()
-    })
-    .then(data => {
-        console.log(data)
-        getWeather(data[0].lat, data[0].lon)
-    })
+        .then((response) => {
+            console.log(response)
+            return response.json()
+        })
+        .then(data => {
+            console.log(data)
+            getWeather(data[0].lat, data[0].lon)
+        })
 }
 
-function getWeather(lat,lon) {
-    var oneCallApi = 'https://api.openweathermap.org/data/2.5/onecall?units=imperial&lat=' +lat+ '&lon='+ lon + '&appid=' + apiKey;
+function getWeather(lat, lon) {
+    var oneCallApi = 'https://api.openweathermap.org/data/2.5/onecall?&units=imperial&lat=' + lat + '&lon=' + lon + '&appid=' + apiKey;
 
     fetch(oneCallApi)
-    .then((response) => {
-        console.log(response)
-        return response.json()
-    })
-    .then(data => {
-        console.log(data)
-        displayCurrentWeather(data)
-    })
+        .then((response) => {
+            console.log(response)
+            return response.json()
+        })
+        .then(data => {
+            console.log(data)
+            displayCurrentWeather(data)
+            displayDailyForecast(data)
+        })
 }
 
 
 
 function displayCurrentWeather(data) {
-        var cityH2 = document.querySelector("#cityName");
-        cityH2.textContent = searchCity.value;
+    var cityH2 = document.querySelector("#cityName");
+    cityH2.textContent = searchCity.value;
 
     var tempH3 = document.querySelector("#temp");
     tempH3.textContent = data.current.temp
 }
 
+function displayDailyForecast(data) {
+    var day1Forecast = document.querySelector('#tomorrowWeather');
+    var city = document.querySelector('.city');
+    city.textContent = searchCity.value;
+    day1Forecast.textContent = data.daily[1].temp.day;
+}
