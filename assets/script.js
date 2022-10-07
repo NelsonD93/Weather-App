3581df2f202cfea1549c16d23fecca0bb55c0a3c
Nelson Drozd <nelsondrozd@gmail.com>
2022-10-06 21:49:45 -0400

    Added UV index images

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,11 @@
-var submitBtn = document.querySelector('#submitBtn');
-var searchCity = document.querySelector('#searchCity');
-var lastSearch = document.querySelector('#lastSearch');
+const submitBtn = document.querySelector('#submitBtn');
+const searchCity = document.querySelector('#searchCity');
+const lastSearch = document.querySelector('#lastSearch');
 var storedCities = localStorage.getItem('lastCity')
-var apiKey = "4024dd0ace3444c4f05da7654e63fece"
-var weekdays = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-var uvImages = document.querySelector('.uvIndex');
+const apiKey = "4024dd0ace3444c4f05da7654e63fece"
+const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const uvImages = document.querySelector('.uvIndex');
+const uvModerate = "<img src=./assets/uvscale/uvi-moderate.jpeg"
 
 
 
@@ -19,7 +20,7 @@ var storedCities = JSON.parse(localStorage.getItem("cities"));
 
 // for (let i = 0; i < array.length; i++) {
 //     const element = array[i];
-    
+
 // }
 
 submitBtn.addEventListener("click", function () {
@@ -59,6 +60,7 @@ function getWeather(lat, lon) {
             console.log(data)
             displayCurrentWeather(data)
             displayDailyForecast(data)
+            displayUV(data)
         })
 }
 
@@ -73,29 +75,30 @@ function displayCurrentWeather(data) {
 }
 
 function displayDailyForecast(data) {
-    var weekday1 = document.querySelector('#weekday1');
-    var weekday2 = document.querySelector('#weekday2');
-    var weekday3 = document.querySelector('#weekday3');
-    var weekday4 = document.querySelector('#weekday4');
-    var weekday5 = document.querySelector('#weekday5');
-    var day1Forecast = document.querySelector('#day1Weather');
-    var day2Forecast = document.querySelector('#day2Weather');
-    var day3Forecast = document.querySelector('#day3Weather');
-    var day4Forecast = document.querySelector('#day4Weather');
-    var day5Forecast = document.querySelector('#day5Weather');
-    // var city = document.querySelector('.city');
-    // Variables for city caption
-    var city1 = document.querySelector('#day1City')
-    var city2 = document.querySelector('#day2City')
-    var city3 = document.querySelector('#day3City')
-    var city4 = document.querySelector('#day4City')
-    var city5 = document.querySelector('#day5City')
-    var uv1 = document.querySelector('#uvIndex1')
-    var uv2 = document.querySelector('#uvIndex2')
-    var uv3 = document.querySelector('#uvIndex3')
-    var uv4 = document.querySelector('#uvIndex4')
-    var uv5 = document.querySelector('#uvIndex5')
-//  Setting content of HTML to searched city
+    let weekday1 = document.querySelector('#weekday1');
+    let weekday2 = document.querySelector('#weekday2');
+    let weekday3 = document.querySelector('#weekday3');
+    let weekday4 = document.querySelector('#weekday4');
+    let weekday5 = document.querySelector('#weekday5');
+    let day1Forecast = document.querySelector('#day1Weather');
+    let day2Forecast = document.querySelector('#day2Weather');
+    let day3Forecast = document.querySelector('#day3Weather');
+    let day4Forecast = document.querySelector('#day4Weather');
+    let day5Forecast = document.querySelector('#day5Weather');
+    // let city = document.querySelector('.city');
+    // letiables for city caption
+    let city1 = document.querySelector('#day1City')
+    let city2 = document.querySelector('#day2City')
+    let city3 = document.querySelector('#day3City')
+    let city4 = document.querySelector('#day4City')
+    let city5 = document.querySelector('#day5City')
+    let today = moment();
+    let day1 = moment().add(1, "days").format('dddd');
+    let day2 = moment().add(2, "days").format('dddd');
+    let day3 = moment().add(3, "days").format('dddd');
+    let day4 = moment().add(4, "days").format('dddd');
+    let day5 = moment().add(5, "days").format('dddd');
+    //  Setting content of HTML to searched city
     city1.textContent = searchCity.value
     city2.textContent = searchCity.value
     city3.textContent = searchCity.value
@@ -108,22 +111,33 @@ function displayDailyForecast(data) {
     day3Forecast.textContent = data.daily[3].temp.day;
     day4Forecast.textContent = data.daily[4].temp.day;
     day5Forecast.textContent = data.daily[5].temp.day;
+    weekday1.textContent = day1;
+    weekday2.textContent = day2;
+    weekday3.textContent = day3;
+    weekday4.textContent = day4;
+    weekday5.textContent = day5;
+}
+
+function displayUV(data) {
+    let uv1 = document.querySelector('#uvIndex1');
+    let uv2 = document.querySelector('#uvIndex2');
+    let uv3 = document.querySelector('#uvIndex3');
+    let uv4 = document.querySelector('#uvIndex4');
+    let uv5 = document.querySelector('#uvIndex5');
     uv1.textContent = 'UV: ' + data.daily[1].uvi;
     uv2.textContent = 'UV: ' + data.daily[2].uvi;
     uv3.textContent = 'UV: ' + data.daily[3].uvi;
     uv4.textContent = 'UV: ' + data.daily[4].uvi;
     uv5.textContent = 'UV: ' + data.daily[5].uvi;
-    weekday1.textContent = moment().utc(data.daily[1].dt).format('dddd')
-    weekday2.textContent = moment().utc(data.daily[2].dt).format('dddd')
-    weekday3.textContent = moment().utc(data.daily[3].dt).format('dddd')
-    weekday4.textContent = moment().utc(data.daily[4].dt).format('dddd')
-    weekday5.textContent = moment().utc(data.daily[5].dt).format('dddd')
+    if (data.daily[1].uvi <= 2) {
+        uv1.textContent = 'low'
+    } else if (data.daily[1].uvi > 2 && data.daily[1].uvi < 5) {
+        uv1.src = uvModerate
+    } else if (data.daily[1].uvi > 5 && data.daily[1].uvi < 8) {
+        uv1.textContent = 'high'
+    } else if (data.daily[1].uvi >= 8 && data.daily[1].uvi <= 10) {
+        uv1.textContent = 'very high'
+    } else {
+        uv1.textContent = 'extreme'
+    }
 }
-
-
-
-// if (data.value === 2 || 1 || 0) {
-    
-// } else {
-    
-// }
