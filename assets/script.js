e1b6ca00a8bf6d24d86cdd6b79ed48c28b95e507
Nelson Drozd <nelsondrozd@gmail.com>
2022-10-07 09:57:56 -0400

    All UV images showing, refactored part of code

--- assets/script.js
+++ assets/script.js
@@ -7,11 +7,11 @@ const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Frida
 const uvImages = document.querySelector('.uvIndex');
 const uvModerate = "/assets/uvscale/uvi-moderate.jpeg"
 const uvI = document.getElementById('uv1');
-uvI.src = "assets/uvscale/uvi-moderate.jpeg"
+const uvI2 = document.getElementById('uv2');
+const uvI3 = document.getElementById('uv3');
+const uvI4 = document.getElementById('uv4');
+const uvI5 = document.getElementById('uv5');
 
-
-
-// var uvScale = 
 
 var cities = [];
 
@@ -19,11 +19,6 @@ localStorage.setItem("cities", JSON.stringify(cities));
 
 //retrieve cities from localstorage
 var storedCities = JSON.parse(localStorage.getItem("cities"));
-
-// for (let i = 0; i < array.length; i++) {
-//     const element = array[i];
-
-// }
 
 submitBtn.addEventListener("click", function () {
     var cityName = searchCity.value;
@@ -62,7 +57,11 @@ function getWeather(lat, lon) {
             console.log(data)
             displayCurrentWeather(data)
             displayDailyForecast(data)
-            displayUV(data)
+            displayUV1(data)
+            displayUV2(data)
+            displayUV3(data)
+            displayUV4(data)
+            displayUV5(data)
         })
 }
 
@@ -82,13 +81,16 @@ function displayDailyForecast(data) {
     let weekday3 = document.querySelector('#weekday3');
     let weekday4 = document.querySelector('#weekday4');
     let weekday5 = document.querySelector('#weekday5');
-    let day1Forecast = document.querySelector('#day1Weather');
-    let day2Forecast = document.querySelector('#day2Weather');
-    let day3Forecast = document.querySelector('#day3Weather');
-    let day4Forecast = document.querySelector('#day4Weather');
-    let day5Forecast = document.querySelector('#day5Weather');
-    // let city = document.querySelector('.city');
-    // letiables for city caption
+    let day1High = document.querySelector('#day1High');
+    let day1Low = document.querySelector('#day1Low');
+    let day2High = document.querySelector('#day2High');
+    let day2Low = document.querySelector('#day2Low');
+    let day3High = document.querySelector('#day3High');
+    let day3Low = document.querySelector('#day3Low');
+    let day4High = document.querySelector('#day4High');
+    let day4Low = document.querySelector('#day4Low');
+    let day5High = document.querySelector('#day5High');
+    let day5Low = document.querySelector('#day5Low');
     let city1 = document.querySelector('#day1City')
     let city2 = document.querySelector('#day2City')
     let city3 = document.querySelector('#day3City')
@@ -106,13 +108,17 @@ function displayDailyForecast(data) {
     city3.textContent = searchCity.value
     city4.textContent = searchCity.value
     city5.textContent = searchCity.value
-    // city.textContent = searchCity.value;
     // Setting content of HTML to temperature
-    day1Forecast.textContent = data.daily[1].temp.day;
-    day2Forecast.textContent = data.daily[2].temp.day;
-    day3Forecast.textContent = data.daily[3].temp.day;
-    day4Forecast.textContent = data.daily[4].temp.day;
-    day5Forecast.textContent = data.daily[5].temp.day;
+    day1High.textContent = 'High: ' + data.daily[1].temp.max + '°F';
+    day1Low.textContent = 'Low: ' + data.daily[1].temp.min + '°F';
+    day2High.textContent = 'High: ' + data.daily[2].temp.max + '°F';
+    day2Low.textContent = 'Low: ' + data.daily[2].temp.min + '°F';
+    day3High.textContent = 'High: ' + data.daily[3].temp.max + '°F';
+    day3Low.textContent = 'Low: ' + data.daily[3].temp.min + '°F';
+    day4High.textContent = 'High: ' + data.daily[4].temp.max + '°F';
+    day4Low.textContent = 'Low: ' + data.daily[4].temp.min + '°F';
+    day5High.textContent = 'High: ' + data.daily[5].temp.max + '°F';
+    day5Low.textContent = 'Low: ' + data.daily[5].temp.min + '°F';
     weekday1.textContent = day1;
     weekday2.textContent = day2;
     weekday3.textContent = day3;
@@ -120,27 +126,72 @@ function displayDailyForecast(data) {
     weekday5.textContent = day5;
 }
 
-function displayUV(data) {
-    let uv1 = document.querySelector('#uvIndex1');
-    let uv2 = document.querySelector('#uvIndex2');
-    let uv3 = document.querySelector('#uvIndex3');
-    let uv4 = document.querySelector('#uvIndex4');
-    let uv5 = document.querySelector('#uvIndex5');
-    uv1.textContent = 'UV: ' + data.daily[1].uvi;
-    uv2.textContent = 'UV: ' + data.daily[2].uvi;
-    uv3.textContent = 'UV: ' + data.daily[3].uvi;
-    uv4.textContent = 'UV: ' + data.daily[4].uvi;
-    uv5.textContent = 'UV: ' + data.daily[5].uvi;
+function displayUV1(data) {
     if (data.daily[1].uvi <= 2) {
-        uv1.textContent = 'low'
+        uvI.src = "assets/uvscale/uvi-low.jpeg"
     } else if (data.daily[1].uvi > 2 && data.daily[1].uvi < 5) {
-        uvI.src
+        uvI.src = "assets/uvscale/uvi-moderate.jpeg"
     } else if (data.daily[1].uvi > 5 && data.daily[1].uvi < 8) {
-        uv1.textContent = 'high'
+        uvI.src = "assets/uvscale/uvi-high.jpeg"
     } else if (data.daily[1].uvi >= 8 && data.daily[1].uvi <= 10) {
-        uv1.textContent = 'very high'
+        uvI.src = "assets/uvscale/uvi-veryhigh.jpeg"
     } else {
-        uv1.textContent = 'extreme'
+        uvI.src = "assets/uvscale/uvi-extreme.jpeg"
     }
 }
 
+function displayUV2(data) {
+    if (data.daily[2].uvi <= 2) {
+        uvI2.src = "assets/uvscale/uvi-low.jpeg"
+    } else if (data.daily[2].uvi > 2 && data.daily[2].uvi < 5) {
+        uvI2.src = "assets/uvscale/uvi-moderate.jpeg"
+    } else if (data.daily[2].uvi > 5 && data.daily[2].uvi < 8) {
+        uvI2.src = "assets/uvscale/uvi-high.jpeg"
+    } else if (data.daily[2].uvi >= 8 && data.daily[2].uvi <= 10) {
+        uvI2.src = "assets/uvscale/uvi-veryhigh.jpeg"
+    } else {
+        uvI2.src = "assets/uvscale/uvi-extreme.jpeg"
+    }
+}
+
+function displayUV3(data) {
+    if (data.daily[3].uvi <= 2) {
+        uvI3.src = "assets/uvscale/uvi-low.jpeg"
+    } else if (data.daily[3].uvi > 2 && data.daily[3].uvi < 5) {
+        uvI3.src = "assets/uvscale/uvi-moderate.jpeg"
+    } else if (data.daily[3].uvi > 5 && data.daily[3].uvi < 8) {
+        uvI3.src = "assets/uvscale/uvi-high.jpeg"
+    } else if (data.daily[3].uvi >= 8 && data.daily[3].uvi <= 10) {
+        uvI3.src = "assets/uvscale/uvi-veryhigh.jpeg"
+    } else {
+        uvI3.src = "assets/uvscale/uvi-extreme.jpeg"
+    }
+}
+
+function displayUV4(data) {
+    if (data.daily[4].uvi <= 2) {
+        uvI4.src = "assets/uvscale/uvi-low.jpeg"
+    } else if (data.daily[4].uvi > 2 && data.daily[4].uvi < 5) {
+        uvI4.src = "assets/uvscale/uvi-moderate.jpeg"
+    } else if (data.daily[4].uvi > 5 && data.daily[4].uvi < 8) {
+        uvI4.src = "assets/uvscale/uvi-high.jpeg"
+    } else if (data.daily[4].uvi >= 8 && data.daily[4].uvi <= 10) {
+        uvI4.src = "assets/uvscale/uvi-veryhigh.jpeg"
+    } else {
+        uvI4.src = "assets/uvscale/uvi-extreme.jpeg"
+    }
+}
+
+function displayUV5(data) {
+    if (data.daily[5].uvi <= 2) {
+        uvI5.src = "assets/uvscale/uvi-low.jpeg"
+    } else if (data.daily[5].uvi > 2 && data.daily[5].uvi < 5) {
+        uvI5.src = "assets/uvscale/uvi-moderate.jpeg"
+    } else if (data.daily[5].uvi > 5 && data.daily[5].uvi < 8) {
+        uvI5.src = "assets/uvscale/uvi-high.jpeg"
+    } else if (data.daily[5].uvi >= 8 && data.daily[5].uvi <= 10) {
+        uvI5.src = "assets/uvscale/uvi-veryhigh.jpeg"
+    } else {
+        uvI5.src = "assets/uvscale/uvi-extreme.jpeg"
+    }
+}
